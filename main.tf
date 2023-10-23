197d1b6cf78d6d60faec914007b79c64830c8ac4
JAG9M <94648415+JAG9M@users.noreply.github.com>
2023-10-23 13:47:15 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,34 +1,40 @@
-## create SSM document
 provider "aws" {
   region = "us-east-1"
+
 }
-
-resource "aws_ssm_document" "MyAnsiblePlaybookdocument" {
-  name          = "ansiblePlaybook_document"
-  document_type = "Command"
-
-  content = jsonencode({
-    schemaVersion = "2.2",
-    description   = "Run Ansible playbook",
-    mainSteps = [
+resource "aws_iam_role" "jenkins_route53_role" {
+  name = "JenkinsRoute53Role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
       {
-        action = "aws:runShellScript",
-        name   = "runShellScript",
-        inputs = {
-          runCommand = [
-            "ansible-playbook /home/ec2-user/ansible/roles/patching/al2/patching.yml"
-          ]
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com" #if jenkins runs on eks
         }
       }
     ]
   })
-}
 
-## Create SSM association
-resource "aws_ssm_association" "MyAnsiblePlaybookassociation" {
-  name = aws_ssm_document.MyAnsiblePlaybookdocument.name
-  targets {
-    key    = "InstanceIds"
-    values = ["i-0f31e5d5ed8f515bb"]
+}
+#Define a policy that includes permissions for Route 53
+data "aws_iam_policy_document" "route53_policy" {
+  statement {
+    actions   = ["route53:ChangeResourceRecordSets", "route53:ListResourceRecordSets"]
+    resources = ["*"]
   }
+}
+#Create an IAM policy
+resource "aws_iam_policy" "jenkins_route53_policy" {
+  name        = "JenkinsRoute53Policy"
+  description = "Policy for Jenkins to interact with Route 53"
+  policy      = data.aws_iam_policy_document.route53_policy.json
+
+}
+# Attach the policy to the IAM role
+resource "aws_iam_role_policy_attachment" "jenkins_route53_policy_attachment" {
+  policy_arn = aws_iam_policy.jenkins_route53_policy.arn
+  role       = aws_iam_role.jenkins_route53_role.name
+
 }
